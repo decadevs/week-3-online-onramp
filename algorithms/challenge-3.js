6db4c36986d6a0af8e5c117635d478b60e637958
OyindamolaK <100429601+OyindamolaK@users.noreply.github.com>
2022-03-13 18:07:55 +0100

    initial commit

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,3 +1,27 @@
-function binaryReversal(value) {}
+function binaryReversal(input) {
+  let result = [0,0,0,0,0,0,0,0];
+  let index = 0;
+  while(input >result.length -1){
+    result.push(0,0,0,0,0,0,0,0);
+  }
 
-module.exports = binaryReversal
+  result[index] = input % 2;
+  input = (input - result[index]) / 2;
+index++;
+}
+let reversed = 0;
+let letOfResult = result.length -1;
+
+for(let i=0; i<letOfResult; i++){
+
+  reversed+=
+  result[i]*Math.pow(2,(letOfResult - i))
+}
+
+return reversed;
+}
+
+console.log(binaryReversal(213));
+console.log(binaryReversal(4567));
+
+

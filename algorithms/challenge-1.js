d2f36830f4ae6a93935d7b970d008947594f3177
Caleb-Oneyemi <caloneyemi@gmail.com>
2022-03-17 12:39:30 +0100

    solution for week 3 algos

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,44 @@
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+  let total = 0
+  let index = 0
+  let input = roman.toLowerCase()
+
+  const converter = {
+    i: 1,
+    v: 5,
+    x: 10,
+    l: 50,
+    c: 100,
+    d: 500,
+    m: 1000,
+  }
+
+  while (index < input.length) {
+    let current = input[index]
+    let next = input[index+1]
+
+    if(converter[current] < converter[next]) {
+      total += converter[next] - converter[current]
+      index += 2
+    } else {
+      total += converter[current]
+      index++
+    }
+  }
+
+  return total
+}
+
+//  while (i < roman.length) {
+//     let cur = roman[i]
+//     let next = roman[i + 1]
+//     if (converter[cur] < converter[next]) {
+//       total += converter[next] - converter[cur]
+//       i += 2
+//     } else {
+//       total += converter[cur]
+//       i++
+//     }
+//   }
 
 module.exports = romanToDecimal
--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,3 +1,14 @@
-function transpose(arr) {}
+function transpose(arr) {
+  const result = []
+
+  for (let row = 0; row < arr.length; row++) {
+    for (let column = 0; column < arr[row].length; column++) {
+      if(!result[column]) result[column] = []
+      result[column][row] = arr[row][column]
+    }
+  }
+
+  return result
+}
 
 module.exports = transpose
